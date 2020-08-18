48c46e1d3b2f98e6ade2d8735b672966d92dfb05
marceloAPP <marcelo_freeday@yahoo.com.br>
2020-08-18 20:07:31 -0300

    Inicio de implementacao do tabuleiro entidades (Tabuleiro, Cor, Peca, Posicao)

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMoviemntos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, int qteMoviemntos, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.qteMoviemntos = qteMoviemntos;
+            this.tab = tab;
+            this.qteMoviemntos = 0;
+        }
+    }
+}
